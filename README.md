b03514c4fc6ac445e9abbd781262f8edf6fea775
Alexander Henry <manebjaelke@Alexanders-MacBook-Air.local>
2021-08-28 11:06:48 +0200

    added plotting of mock data

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Project implementing Saxo's API for practice and eventually hook into my own account. 
 
 Using demo token from [their dev protal](https://www.developer.saxo/openapi/learn)
+
+Remember to chmod u+x the bash script to run easily with the input arguements.
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import sys
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import date, timedelta
+
 
 def getAPI(url, params, headers,):
     Request = requests.get(url,params=params,headers=headers)
@@ -21,6 +26,23 @@ Parameters = {'AccountKey': AccountKey, 'ClientKey': ClientKey}
 
 GetAccountPath = "/port/v1/balances"
 
-AccountInfo = getAPI(BaseUrl+GetAccountPath,Parameters,Headers)
+#AccountInfo = getAPI(BaseUrl+GetAccountPath,Parameters,Headers)
 
-print(AccountInfo.get("TotalValue"))
+#print(AccountInfo.get("TotalValue"))
+
+GetAccountHistoryPath = "/hist/v3/accountvalues/"+ClientKey
+
+MockParam = {"MockDataId":"001"}
+AccountHistory = getAPI(BaseUrl+GetAccountHistoryPath,MockParam,Headers).get("Data")
+
+today = date.today()
+lastMonth = today - timedelta(days=30)
+lastYear = today - timedelta(days=365)
+
+for account in AccountHistory:
+    values =  [account["AccountValue"],account["AccountValueMonth"],account["AccountValueYear"]]
+    dates = [lastYear, lastMonth, today]
+    plt.plot(dates,values)
+plt.show()
+exit()
+
